03e317eb0aae257a2341fc46cd8240e8b30aa798
kpuduls <84714429+kpuduls@users.noreply.github.com>
2022-02-15 09:49:34 +0200

    Update definitions.py

--- automation/definitions.py
+++ automation/definitions.py
@@ -1,6 +1,4 @@
 # Global definitions
 
-# Competition name
-
-comp_name = "g-research-crypto-forecasting"
-comp_path = "C:\\Users\\kpudu01\\AppData\\Local\\Programs\\Git\\data-science-pipeline\\competitions"
+# Competitions dir
+comp_path = "C:\Temp\pipeline\data-science-pipeline\competitions"
